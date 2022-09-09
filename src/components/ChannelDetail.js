10bf93030cc20ed406d06ef8efe6a807b0f12517
abcpqr-123 <surya.gaonkar.signup@gmail.com>
2022-09-10 01:20:36 +0530

    after completing the searchbar - 1

--- src/components/ChannelDetail.js
+++ src/components/ChannelDetail.js
@@ -45,43 +45,41 @@ export default ChannelDetail
 
 
 
-{/* // const ChannelDetail = () => {
-//     // const [channelDetail, setChannelDetail] = useState()
-//       const [channelDetail, setChannelDetail] = useState(null)
-//     const [videos, setVideos] = useState([])
+// {/* // const ChannelDetail = () => {
+// //     // const [channelDetail, setChannelDetail] = useState()
+// //       const [channelDetail, setChannelDetail] = useState(null)
+// //     const [videos, setVideos] = useState([])
 
-//     const {id} = useParams();
+// //     const {id} = useParams();
 
-//     console.log(channelDetail, videos)
+// //     console.log(channelDetail, videos)
 
-//     useEffect(() => {
-//       fetchFromAPI(`channels?part="snippet&id=${id}`).then((data) => setChannelDetail(data?.items[0]))
+// //     useEffect(() => {
+// //       fetchFromAPI(`channels?part="snippet&id=${id}`).then((data) => setChannelDetail(data?.items[0]))
 
-//       fetchFromAPI(`search?channelId=${id}&part="snippet&order=date`).then((data) => setVideos(data?.items))
-//     }, [id])
+// //       fetchFromAPI(`search?channelId=${id}&part="snippet&order=date`).then((data) => setVideos(data?.items))
+// //     }, [id])
 
-//   return(
-//     <Box  minHeight="95vh">
-//       <Box>
-//         <div style={{
-//           background: 'linear-gradient(90deg, rgba(2,0,36,1) 0%, rgba(50,9,121,1) 0%, rgba(0,212,255,1) 100%)',
-//         zIndex:10,
-//         height: '300px'
-//         }}
-//          />
-//          <ChannelCard channelDetail={channelDetail} marginTop='-110px'/>
-//       </Box>
-//       <Box display="flex" p = "2">
-//         <Box sx={{mr: {sm: '100px'}}} />
-//           <Videos videos={videos} />
-//         </Box>
-//       </Box>
-//   )
-// }
+// //   return(
+// //     <Box  minHeight="95vh">
+// //       <Box>
+// //         <div style={{
+// //           background: 'linear-gradient(90deg, rgba(2,0,36,1) 0%, rgba(50,9,121,1) 0%, rgba(0,212,255,1) 100%)',
+// //         zIndex:10,
+// //         height: '300px'
+// //         }}
+// //          />
+// //          <ChannelCard channelDetail={channelDetail} marginTop='-110px'/>
+// //       </Box>
+// //       <Box display="flex" p = "2">
+// //         <Box sx={{mr: {sm: '100px'}}} />
+// //           <Videos videos={videos} />
+// //         </Box>
+// //       </Box>
+// //   )
+// // }
 
-// export default ChannelDetail
-
-
+// // export default ChannelDetail
 
 
 
@@ -90,12 +88,14 @@ export default ChannelDetail
 
 
 
-// import React from 'react'
 
-// const ChannelDetail = () => {
-//   return (
-//     <div></div>
-//   )
-// }
 
-// export default ChannelDetail */}
+// // import React from 'react'
+
+// // const ChannelDetail = () => {
+// //   return (
+// //     <div></div>
+// //   )
+// // }
+
+// // export default ChannelDetail */}
