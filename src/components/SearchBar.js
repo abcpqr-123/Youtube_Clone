00ea8ec51c19fd8e692ed95a915670d421849b66
abcpqr-123 <surya.gaonkar.signup@gmail.com>
2022-09-10 01:20:51 +0530

    src/components/SearchBar.js

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -3,11 +3,25 @@ import {useNavigate} from 'react-router-dom'
 import {Paper, IconButton} from '@mui/material'
 import { Search } from '@mui/icons-material'
 
-const SearchBar = ({select}) => {
+const SearchBar = () => {
+  const [searchTerm, setSearchTerm] = useState('')
+  
+  const navigate = useNavigate()
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+
+
+  if (searchTerm) {
+    navigate(`/search/${searchTerm}`)
+
+    setSearchTerm('');
+  }
+}
   return (
     <Paper 
     component="form"
-    onSubmit = {() => {}}
+    onSubmit = {handleSubmit}
     sx={{
       borderRadius: 20,
       border: '1px solid #e3e3e3',
@@ -19,8 +33,8 @@ const SearchBar = ({select}) => {
     <input 
        className='search-bar'
        placeholder='Search...'
-       value=""
-       onChange={() => {}}
+       value={searchTerm}
+       onChange={(e) => setSearchTerm(e.target.value)}
        />
        <IconButton type="submit" sx={{ p:'10px', color: 'red'}}>
         <Search />
