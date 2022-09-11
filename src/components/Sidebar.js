422fb926f14762bdbd79cbc7f24d33ea631ff6b0
abcpqr-123 <surya.gaonkar.signup@gmail.com>
2022-09-11 12:48:21 +0530

    fix-color-final

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -21,11 +21,11 @@ const  Sidebar = ({ selectedCategory, setSelectedCategory }) => (
     className='category-btn'
     onClick={() => setSelectedCategory(category.name) }
     style={{
-        background: category.name === selectedCategory && '#FC1503', color:'white'
+        background: category.name === selectedCategory && 'blue', color:'white'
     }}
     key={category.name}
     >   
-        <span style={{color: category.name === selectedCategory ? 'white' : 'red', marginRight: '15px'}}>
+        <span style={{color: category.name === selectedCategory ? 'white' : 'blue', marginRight: '15px'}}>
         {category.icon}</span>
         <span style={{opacity: category.name === selectedCategory ? '1': '0.8'}}>{category.name}</span>
     </button>
